d1b7d9a5faaae6bdabd4f23ea5505b08aada7581
Pteeradet <118308986+Pteeradet@users.noreply.github.com>
2023-01-16 00:23:41 +0700

    Add files via upload

---
+++ LAB002/2-10.py
@@ -0,0 +1,6 @@
+fullname = "Teeradet Prasertwongpana"
+print(fullname)
+
+animals = ['dog', 'cat', 'fish', 'bird', 'snake']
+for i in range(0, len(animals)):
+    print(animals[i].upper(), end =' ')
---
+++ LAB002/2-8.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+   $name = "Teeradet";
+   echo "$name go to school";
+   echo "<br/>";
+   echo var_dump($name);
+?> 
+
+</body>
+</html>
